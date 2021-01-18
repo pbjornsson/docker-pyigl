feff014b1179e9a1c9ee0f69569275f3d4dc05e7
pbjornsson <pallbjornsson15@gmail.com>
2021-01-18 10:49:13 +0000

    Update run_tutorial.sh

--- run_tutorial.sh
+++ run_tutorial.sh
@@ -3,5 +3,5 @@
 script_dir="`cd $(dirname $0); pwd`"
 $script_dir/glrun.sh -i gilureta/pyigl -p 6081 -d "--workdir /setup/libigl/python/" -r "$@"
 # If you use docker-machine use this line instead 
-# $script_dir/glrun.sh -m -i gilureta/pyigl -p 6081 -d "--workdir /setup/libigl/python/" -r "$@"
+$script_dir/glrun.sh -m -i gilureta/pyigl -p 6081 -d "--workdir /setup/libigl/python/" -r "$@"
 
